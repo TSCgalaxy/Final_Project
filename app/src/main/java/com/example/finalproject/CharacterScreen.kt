3217d4be560b543eebc2bffa2c0c93e08c1eb889
SquishyHorse <102880288+SquishyHorse@users.noreply.github.com>
2023-04-03 16:31:43 -0500

    Created CharacterEntity.kt

--- app/src/main/java/com/example/finalproject/CharacterScreen.kt
+++ app/src/main/java/com/example/finalproject/CharacterScreen.kt
@@ -2,6 +2,7 @@ package com.example.finalproject
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 /**
  * Character Creation Screen
@@ -11,4 +12,10 @@ fun CharacterScreen() {
     Column() {
 
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CharacterScreenPreview() {
+
 }
