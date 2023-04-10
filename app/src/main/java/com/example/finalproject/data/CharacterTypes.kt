13d43285bd5f84b85ae98187404ea90cb53c8bfb
SquishyHorse <102880288+SquishyHorse@users.noreply.github.com>
2023-04-10 16:06:12 -0500

    Updated CharacterScreen.kt, Added Race/Class/Attribute definitions in CharacterTypes.kt

---
+++ app/src/main/java/com/example/finalproject/data/CharacterTypes.kt
@@ -0,0 +1,38 @@
+package com.example.finalproject.data
+
+//Classes
+val characterClasses: Array<String> = arrayOf(
+    "Barbarian",
+    "Bard",
+    "Cleric",
+    "Druid",
+    "Fighter",
+    "Monk",
+    "Paladin",
+    "Ranger",
+    "Rogue",
+    "Sorcerer",
+    "Warlock",
+    "Wizard")
+
+//Races
+val characterRaces: Array<String> = arrayOf(
+    "Dragonborn",
+    "Dwarf",
+    "Elf",
+    "Gnome",
+    "Half-Elf",
+    "Halfling",
+    "Half-Orc",
+    "Human",
+    "Tiefling")
+
+//Attribute Names
+val characterAttributes: Array<String> = arrayOf(
+    "Strength",
+    "Constitution",
+    "Dexterity",
+    "Intelligence",
+    "Wisdom",
+    "Charisma"
+)
