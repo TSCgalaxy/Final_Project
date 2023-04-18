aba55305c042edaa19c4ed4767693c3ab83a1823
SquishyHorse <102880288+SquishyHorse@users.noreply.github.com>
2023-04-18 13:51:45 -0500

    Updated CharacterScreen.kt

--- app/src/main/java/com/example/finalproject/CharacterScreen.kt
+++ app/src/main/java/com/example/finalproject/CharacterScreen.kt
@@ -1,28 +1,29 @@
 package com.example.finalproject
 
-import android.widget.EditText
-import android.widget.GridLayout
-import android.widget.GridView
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,7 +31,6 @@ import androidx.compose.ui.unit.toSize
 import com.example.finalproject.data.characterAttributes
 import com.example.finalproject.data.characterClasses
 import com.example.finalproject.data.characterRaces
-import java.time.format.TextStyle
 
 @Composable
 fun TextFieldColorOverride(
@@ -105,51 +105,164 @@ fun CharacterDropdown (items: Array<String>,
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun CharacterTextField(@StringRes placeholder: Int, @StringRes label: Int) {
+fun CharacterTextField(
+    @StringRes placeholder: Int,
+    @StringRes label: Int,
+    modifier: Modifier = Modifier,
+    isAttr: Boolean = false,
+    attrConstraint: Int = 20) {
+
     //Save the state of the text box's contents.
     var text by remember {mutableStateOf("")}
+    var txtStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Start)
+    var kbOptions = KeyboardOptions(keyboardType = KeyboardType.Text, imeAction = ImeAction.Done)
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
+
+    //If the textbox is intended to be for an attribute, center align the text
+    if (isAttr) {
+        txtStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center, fontSize = 32.sp)
+        kbOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done)
+    }
+
 
     //Create TextField
     TextField(text,
-        onValueChange = {text = it},
-        placeholder = { Text(text = stringResource(id = placeholder)) },
+        onValueChange = {
+            //Check for non number characters
+            if (isAttr) {
+                if (it.isEmpty()) {
+                    text = ""
+                }
+                else if (it.matches(Regex("^\\d*\$")) && it.toInt() <= attrConstraint) {
+                    text = it
+                }
+            } else {
+                text = it
+            }},
+        placeholder = { Text(textAlign = TextAlign.Center, text = stringResource(id = placeholder)) },
         label = { Text(text = stringResource(id = label)) },
+        modifier = modifier,
+        textStyle = txtStyle,
+        keyboardOptions = kbOptions,
+        keyboardActions = KeyboardActions(
+            onDone = {
+                keyboardController?.hide()
+                focusManager.clearFocus() })
     )
 }
-
 
 /**
  * Character Creation Screen
  */
 @Composable
-fun CharacterScreen(title: String,
-                    modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .padding(4.dp)
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
+fun CharacterScreen(
+    title: String,
+    modifier: Modifier = Modifier,
+    onSubmit: () -> Unit = {}) {
 
-        //Title
-        Text(text = title, fontSize = 24.sp)
-        CharacterTextField(placeholder = R.string.placeholder_enter_name, label = R.string.label_name)
-        Column(Modifier.padding(8.dp)) {
-            CharacterDropdown(items = characterClasses,
-                placeholder = R.string.placeholder_select_class,
-                label = R.string.label_class)
-            CharacterDropdown(items = characterRaces,
-                placeholder = R.string.placeholder_select_race,
-                label = R.string.label_race )
-        }
+    LazyColumn() {
 
-        LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 128.dp)) {
-            characterAttributes.forEach() {it ->
-                item {
-                    Text(it)
+    item {
+
+        Column(
+            modifier = modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+
+            //Title
+            Text(text = title, fontSize = 24.sp)
+
+            //Name
+            CharacterTextField(
+                placeholder = R.string.placeholder_enter_name,
+                label = R.string.label_name
+            )
+
+            //Race and Class
+            Column(Modifier.padding(8.dp)) {
+                CharacterDropdown(
+                    items = characterClasses,
+                    placeholder = R.string.placeholder_select_class,
+                    label = R.string.label_class
+                )
+
+                //Spacer
+                Spacer(Modifier.padding(4.dp))
+
+                CharacterDropdown(
+                    items = characterRaces,
+                    placeholder = R.string.placeholder_select_race,
+                    label = R.string.label_race
+                )
+            }
+
+            //Level and XP
+            Row(Modifier.fillMaxWidth(1f)) {
+                CharacterTextField(
+                    placeholder = R.string.placeholder_select_level,
+                    label = R.string.label_level,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(.5f),
+                    isAttr = true,
+                    attrConstraint = 20
+                )
+                CharacterTextField(
+                    placeholder = R.string.placeholder_select_xp,
+                    label = R.string.label_XP,
+                    modifier = Modifier.padding(8.dp),
+                    isAttr = true,
+                    attrConstraint = 999999
+                )
+            }
+
+            //ATTRIBUTES
+            for (i in 0..5 step 2) {
+                Row(
+                    Modifier
+                        .fillMaxWidth(1f)
+                        .align(Alignment.CenterHorizontally)
+                ) {
+                    CharacterTextField(
+                        placeholder = characterAttributes[i].second,
+                        label = characterAttributes[i].first,
+                        modifier = Modifier
+                            .height(128.dp)
+                            .padding(4.dp)
+                            .fillMaxWidth(.5f),
+                        isAttr = true
+                    )
+                    CharacterTextField(
+                        placeholder = characterAttributes[i + 1].second,
+                        label = characterAttributes[i + 1].first,
+                        modifier = Modifier
+                            .height(128.dp)
+                            .padding(4.dp),
+                        isAttr = true
+                    )
                 }
+            }
+
+            //DESCRIPTION
+            CharacterTextField(
+                placeholder = R.string.placeholder_char_desc,
+                label = R.string.label_char_desc,
+                modifier = Modifier
+                    .fillMaxWidth(.8f)
+                    .height(128.dp)
+                )
+
+            //Submit button
+            Button(onClick = {}) {
+                Text(text = stringResource(id = R.string.button_submit))
+            }
+
             }
         }
     }
