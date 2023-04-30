acee22a53d166a0567f513a8319afe13df982834
PaytonNess <38738167+PaytonNess@users.noreply.github.com>
2023-04-30 15:12:23 -0500

    removeSql

--- app/src/main/java/com/example/finalproject/data/CharacterDB.kt
+++ app/src/main/java/com/example/finalproject/data/CharacterDB.kt
@@ -43,7 +43,7 @@ abstract class CharacterDB: RoomDatabase() {
             }
 
             override fun onOpen(db: SupportSQLiteDatabase) {
-                db.execSQL("SELECT * FROM item;")
+
             }
         }
 
